01c8cda93e8446aa428e3b456e836142b530c5d2
sbogutyn <sbogutyn@sigma.ug.edu.pl>
2010-10-11 14:15:50 +0200

    poprawki w kodzie afinicznego

--- afiniczny/afine.py
+++ afiniczny/afine.py
@@ -4,20 +4,19 @@ from optparse import OptionParser
 
 # szyfruje pojedyncza litera afinicznym (cezar to afiniczny z a = 1)
 def zaszyfruj_litere(litera,a,b):
+	litery = None
 	if (litera >= 'a' and litera <= 'z'):
-		kod = list(ascii_lowercase).index(litera)
-		nowy_kod = (kod * a + b) % 26
-		return list(ascii_lowercase)[nowy_kod]
+		litery = ascii_lowercase
 	elif (litera >= 'A' and litera <= 'Z'):
-		kod = list(ascii_uppercase).index(litera)
-		nowy_kod = (kod * a + b) % 26
-		return list(ascii_uppercase)[nowy_kod]
+		litery = ascii_uppercase
 	else:
 		return litera;
+	kod = list(litery).index(litera)
+	nowy_kod = (kod * a + b) % 26
+	return list(litery)[nowy_kod]
 	
 def zaszyfruj_alfabet(a, b):
-	szyfr = lambda x : zaszyfruj_litere(x, a, b);
-	return ''.join(map(szyfr, list(letters)))
+	return ''.join(map(lambda x : zaszyfruj_litere(x, a, b), list(letters)))
 
 def znajdz_odwrotnosc(a, modn):
 	for i in xrange(modn):
@@ -33,33 +32,29 @@ def sprawdz_odwrotnosc(a, b, modn):
 
 def odszyfruj_litere(litera, a, b):
 	odw_a = znajdz_odwrotnosc(a, 26)
+	litery = None
 	if odw_a != -1:
 		if (litera >= 'A' and litera <= 'Z'):
-			kod = list(ascii_uppercase).index(litera)
-			nowy_kod = (odw_a * (kod - b)) % 26
-			return list(ascii_uppercase)[nowy_kod]
+			litery = ascii_uppercase
 		elif (litera >= 'a' and litera <= 'z'):
-			kod = list(ascii_lowercase).index(litera)
-			nowy_kod = (odw_a * (kod - b)) % 26
-			return list(ascii_lowercase)[nowy_kod]
+			litery = ascii_lowercase
 		else:
 			return litera
+		kod = list(litery).index(litera)
+		nowy_kod = (odw_a * (kod - b)) % 26
+		return list(litery)[nowy_kod]
 
 def odszyfruj_alfabet(alfabet, a, b):
-	odszyfruj = lambda x : odszyfruj_litere(x, a, b)
-	return ''.join(map(odszyfruj, list(alfabet)))
+	return ''.join(map(lambda x : odszyfruj_litere(x, a, b), list(alfabet)))
 
 def tlumacz_tekst(tekst, alfabet1, alfabet2):
-	transtab = maketrans(alfabet1, alfabet2)
-	return tekst.translate(transtab)
+	return tekst.translate(maketrans(alfabet1, alfabet2))
 
 def zaszyfruj_tekst(tekst, a, b):
-	zaszyfrowany_alfabet = zaszyfruj_alfabet(a, b)
-	return tlumacz_tekst(tekst,letters, zaszyfrowany_alfabet)
+	return tlumacz_tekst(tekst,letters, zaszyfruj_alfabet(a, b))
 
 def odszyfruj_tekst(tekst, a, b):
-	zaszyfrowany_alfabet = zaszyfruj_alfabet(a, b)
-	return tlumacz_tekst(tekst, zaszyfrowany_alfabet, letters)
+	return tlumacz_tekst(tekst, zaszyfruj_alfabet(a, b), letters)
 
 def nwd(a, b):
 	x = a
@@ -132,83 +127,59 @@ if __name__ == "__main__":
 	if len(args) != 0:
 		parser.error("zla liczba argumentow")
 
+	a = b = 0
 	if options.afiniczny_flag:
-		if options.szyfruj_flag:
-			print "Szyfrowanie afinicznym"
-			klucze = wczytaj_klucz(PLIK_KEY)
-			tekst = odczytaj_plik(PLIK_PLAIN)
-			zapisz_do_pliku(PLIK_CRYPTO, zaszyfruj_tekst(tekst, klucze[0], klucze[1]))
-		elif options.deszyfruj_flag:
-			print "Deszyfrowanie afinicznym"
-			klucze = wczytaj_klucz(PLIK_KEY)
-			zaszyfrowany_tekst = odczytaj_plik(PLIK_CRYPTO)
-			zapisz_do_pliku(PLIK_DECRYPT, odszyfruj_tekst(zaszyfrowany_tekst, klucze[0], klucze[1]))
-		elif options.jawny_flag:
-			print "kryptoanaliza z tekstem jawnym"
-			pomoc = odczytaj_plik(PLIK_EXTRA).strip()
-			zaszyfrowany_tekst = odczytaj_plik(PLIK_CRYPTO)
-			if (len(pomoc) < len(zaszyfrowany_tekst)):
-				dlugosc = len(pomoc)
-			else:
-				dlugosc = len(zaszyfrowany_tekst)
-			wyniki = set()
-			for i in xrange(dlugosc):
-				wynik = set()
-				for a in range(26):
-					if nwd (a, 26) == 1:
-						for b in range(26):
-							if odszyfruj_litere(zaszyfrowany_tekst[i], a ,b) == pomoc[i]:
-								wynik.add( (a, b) )
-				if len(wyniki) == 0:
-					for i in wynik:
-						wyniki.add(i)
-				else:
-					print "wyniki = ", wyniki, " &  ", wynik
-					wyniki = wyniki & wynik
-			print pomoc[dlugosc - 1]
-			print zaszyfrowany_tekst[dlugosc - 1]
-			print wyniki
-
-			
-
-		elif options.krypto_flag:
-			zaszyfrowany_tekst = odczytaj_plik(PLIK_CRYPTO)
-			tekst = ""
-			for a in range(26):
-				if nwd(a, 26) == 1:
-					for b in range(26):
-						tekst += odszyfruj_tekst(zaszyfrowany_tekst, a, b) + "\n"
-			zapisz_do_pliku(PLIK_DECRYPT, tekst)
-
-			print "kryptoanaliza za pomoca kryptogramu"
-	
+		(a, b) = wczytaj_klucz(PLIK_KEY)
 	elif options.cezar_flag:
-		print "cezar!"
-		if options.szyfruj_flag:
-			print "szyfrowanie"
-		elif options.deszyfruj_flag:
-			print "deszyfrowanie"
-		elif options.jawny_flag:
-			print "kryptoanaliza z tekstem jawnym"
-		elif options.krypto_flag:
-			print "kryptoanaliza za pomoca kryptogramu"
+		a, b = 1 , wczytaj_klucz(PLIK_KEY)[0]
+	else:
+		print "Musisz wybrac szyfr afiniczny lub cezara"
+
+	if options.szyfruj_flag:
+		print "Szyfrowanie afinicznym"
+		klucze = wczytaj_klucz(PLIK_KEY)
+		tekst = odczytaj_plik(PLIK_PLAIN)
+		zapisz_do_pliku(PLIK_CRYPTO, zaszyfruj_tekst(tekst, a, b))
+	elif options.deszyfruj_flag:
+		print "Deszyfrowanie afinicznym"
+		klucze = wczytaj_klucz(PLIK_KEY)
+		zaszyfrowany_tekst = odczytaj_plik(PLIK_CRYPTO)
+		zapisz_do_pliku(PLIK_DECRYPT, odszyfruj_tekst(zaszyfrowany_tekst, a, b))
+	elif options.jawny_flag:
+		print "kryptoanaliza z tekstem jawnym"
+		pomoc = odczytaj_plik(PLIK_EXTRA).strip()
+		zaszyfrowany_tekst = odczytaj_plik(PLIK_CRYPTO)
+		if (len(pomoc) < len(zaszyfrowany_tekst)):
+			dlugosc = len(pomoc)
 		else:
-			print "Klucze: ", wczytaj_klucz(PLIK_KEY)
-			print zaszyfruj_litere('A', 1, 1)
-			print zaszyfruj_litere('B', 2, 1)
-			print zaszyfruj_litere('Z', 1, 1)
-			print zaszyfruj_alfabet(1,1)
-			for i in range(1, 25):
-				if nwd(i, 26) == 1:
-					print "a = ", i
-					print "Zaszyfrowany: ", zaszyfruj_tekst("ala ma kota", i, 1)
-					print "Odszyfrowany: ", odszyfruj_tekst(zaszyfruj_tekst("ala ma kota", i, 1), i, 1)
-			try:
-				print odszyfruj_alfabet(zaszyfruj_alfabet(26, 3), 26, 3)
-			except TypeError:
-				print "NWD(a, 26) != 1"
-			try:
-				for i in xrange(26):
-					print "Odwrotnosc: ", i , " to: ", znajdz_odwrotnosc(i ,26)
-			except TypeError:
-				print "brak odwrotnosci"
+			dlugosc = len(zaszyfrowany_tekst)
+		wyniki = set()
+		for i in xrange(dlugosc):
+			wynik = set()
+			for a in range(26):
+				if nwd (a, 26) == 1:
+					for b in range(26):
+						if odszyfruj_litere(zaszyfrowany_tekst[i], a ,b) == pomoc[i]:
+							wynik.add( (a, b) )
+			if len(wyniki) == 0:
+				for i in wynik:
+					wyniki.add(i)
+			else:
+				wyniki = wyniki & wynik
+		if len(wyniki) == 1:
+			(a, b) = wyniki.pop()
+			tekst = odszyfruj_tekst(zaszyfrowany_tekst, a, b)
+			zapisz_do_pliku(PLIK_DECRYPT, tekst)
+		
+
+	elif options.krypto_flag:
+		zaszyfrowany_tekst = odczytaj_plik(PLIK_CRYPTO)
+		tekst = ""
+		for x in range(26):
+			if nwd(x , 26) == 1:
+				for y in range(26):
+					tekst += odszyfruj_tekst(zaszyfrowany_tekst, x , y) + "\n"
+		zapisz_do_pliku(PLIK_DECRYPT, tekst)
+
+		print "kryptoanaliza za pomoca kryptogramu"
+	
---
+++ afiniczny/test_afine.py
@@ -0,0 +1,27 @@
+import unittest
+import afine
+
+class TestAfiniczneSzyfrowanie(unittest.TestCase):
+	
+	def setup(self):
+		pass
+
+	def test_szyfruj_litere(self):
+		self.assertEqual('b', afine.zaszyfruj_litere('a', 1, 1))
+	
+	def test_bledne_szyfrowanie_litery(self):
+		self.assertNotEqual('b', afine.zaszyfruj_litere('a', 5, 5))
+	
+	def test_szyfruj_tekst(self):
+		self.assertEqual("bcd", afine.zaszyfruj_tekst("abc", 1, 1))
+	
+	def test_nwd(self):
+		self.assertEqual(1, nwd(1, 26))
+		self.assertEqual(1, nwd(3, 26))
+		self.assertEqual(1, nwd(5, 26))
+	
+
+
+
+if __name__ == "__main__":
+	unittest.main()
